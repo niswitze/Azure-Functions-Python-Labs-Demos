3acd87f638012b8198dff18a7b34390bb7450369
Nicholas Switzer <niswitze@microsoft.com>
2020-10-07 15:57:37 -0500

    DEMO CODE: Adding complete version of intial demos for a basic Azure Python Function and integrating application insights using OpenCensus

---
+++ Demos/Monitoring Demo/CustomLogsHttpTrigger/__init__.py
@@ -0,0 +1,32 @@
+import logging
+
+import azure.functions as func
+
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+
+logger = logging.getLogger(__name__)
+
+logger.addHandler(AzureLogHandler())
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        logger.warning(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        logger.warning("This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.")
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
